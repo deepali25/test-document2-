e53bffcf51bc099d695c9af62f55386f66c90724
sanjeev <sanjeevsliet@gmail.com>
2015-02-27 16:26:51 +0530

    getting ready for the next merge conflict

--- deepak1.c
+++ deepak1.c
@@ -4,6 +4,6 @@ void main()
 {
 	printf("my name is deepak\n");
 	printf("and i am 21 years old\n");
-	printf("great will be the working");
+	printf("great will be the working and so will be the earning\n");
 }
 
