8e39a22db0ec94997bc330d55b2864eaa4663c1e
deepali25 <deepamdeebu@gmail.com>
2015-03-04 10:52:22 +0530

    Update deepak1.c

--- deepak1.c
+++ deepak1.c
@@ -4,7 +4,7 @@ void main()
 {
 	printf("my name is deepak\n");
 	printf("and i am 21 years old\n");
-	printf("great will be the working and so will be the earning\n");
+	printf("great is the working and so will be the earning\n");
 	printf("testing a code");
 }
 
